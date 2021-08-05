566f8429f3c6a659512f33c59478005e34a3fa62
Majoxs <majoxs@gmail.com>
2021-08-06 00:32:40 +0500

    feat(script.js): solution of the task

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const isNumber = function (n) {
 
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-function guessTheNumber(randomNumber) {
+const guessTheNumber = function (randomNumber) {
    let userNumber = prompt('Угадай число от 1 до 100');
 
    if (userNumber === null) {
