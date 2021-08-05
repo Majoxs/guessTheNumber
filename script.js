61ca3ffb160857eed71d06f3e08c75d2c4510fab
Majoxs <majoxs@gmail.com>
2021-08-06 00:22:41 +0500

    feat(script.js): solution of the task

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const isNumber = function (n) {
+   return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+function guessTheNumber(randomNumber) {
+   let userNumber = prompt('Угадай число от 1 до 100');
+
+   if (userNumber === null) {
+      alert('Игра окончена.');
+      return;
+   } else if (isNumber(userNumber)) {
+      if (userNumber < randomNumber) {
+         alert('Загаданное число больше');
+      } else if (userNumber > randomNumber) {
+         alert('Загаданное число меньше');
+      } else if (userNumber == randomNumber) {
+         alert('Поздравляю, Вы угадали!!!');
+         return;
+      }
+   } else {
+      alert('Введи число!');
+   }
+
+   console.log('Число пользователя: ' + userNumber, typeof userNumber);
+   console.log('Загаданное число: ' + randomNumber, typeof randomNumber);
+
+   guessTheNumber(randomNumber);
+}
+
+guessTheNumber(randomNumber);
+
+
+
+
