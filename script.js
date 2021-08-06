dfeea9da057f39be67500bf2625e478b8ed19e50
Majoxs <majoxs@gmail.com>
2021-08-06 10:25:23 +0500

    feat(script.js):solution of the task

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 'use strict';
 
-const isNumber = function (n) {
-   return !isNaN(parseFloat(n)) && isFinite(n);
-};
+const guessTheNumber = function () {
 
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+   const isNumber = function (n) {
+      return !isNaN(parseFloat(n)) && isFinite(n);
+   };
+   const randomNumber = Math.floor(Math.random() * 100) + 1;
 
-const guessTheNumber = function (randomNumber) {
-   let userNumber = prompt('Угадай число от 1 до 100');
+   const compare = function (randomNumber) {
 
-   if (userNumber === null) {
-      alert('Игра окончена.');
-      return;
-   } else if (isNumber(userNumber)) {
-      if (userNumber < randomNumber) {
-         alert('Загаданное число больше');
-      } else if (userNumber > randomNumber) {
-         alert('Загаданное число меньше');
-      } else if (userNumber == randomNumber) {
-         alert('Поздравляю, Вы угадали!!!');
+      let userNumber = prompt('Угадай число от 1 до 100');
+
+      if (userNumber === null) {
+         alert('Игра окончена.');
          return;
+      } else if (isNumber(userNumber)) {
+         if (userNumber < randomNumber) {
+            alert('Загаданное число больше');
+         } else if (userNumber > randomNumber) {
+            alert('Загаданное число меньше');
+         } else if (userNumber == randomNumber) {
+            alert('Поздравляю, Вы угадали!!!');
+            return;
+         }
+      } else {
+         alert('Введи число!');
       }
-   } else {
-      alert('Введи число!');
+
+      console.log('Число пользователя: ' + userNumber, typeof userNumber);
+      console.log('Загаданное число: ' + randomNumber, typeof randomNumber);
+
+      compare(randomNumber);
    }
-
-   console.log('Число пользователя: ' + userNumber, typeof userNumber);
-   console.log('Загаданное число: ' + randomNumber, typeof randomNumber);
-
-   guessTheNumber(randomNumber);
+   compare(randomNumber);
 }
 
-guessTheNumber(randomNumber);
-
-
-
-
+guessTheNumber();
